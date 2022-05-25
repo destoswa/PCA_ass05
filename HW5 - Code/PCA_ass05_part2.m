e2d7ca4b7485ed8c3cfe0de399295d21f82e4a5e
destoswa <84578314+destoswa@users.noreply.github.com>
2022-05-26 01:56:27 +0200

    f

--- HW5 - Code/PCA_ass05_part2.m
+++ HW5 - Code/PCA_ass05_part2.m
@@ -1,8 +1,15 @@
 clear all
 close all
-
+%% data
+[Dp, dDp, ndist] = generate_sizehist;
+gradp = [1e-12 1e-11 1e-10 1e-9]; %[atm]
+N0 = 200; %[cm^-3]
+N = [50,200,500]; %[cm^-3]
+gradp0 = 1e-9; %[atm]
 %% Computation of Tau_{k}^{coag}
-
+tao_coag = ndist./coag_loss_coef(ndist,Dp);
+figure(1)
+semilogy(Dp,tao_coag,'.')
 %% Computation of Tao_{k,k+1}^{cond}
 
 %% Computation of Pr_{k->k+1}
