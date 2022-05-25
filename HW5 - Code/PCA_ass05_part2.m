d761613dfe542f6cfd220baba5e3175e7776d64b
destoswa <84578314+destoswa@users.noreply.github.com>
2022-05-25 21:46:42 +0200

    asdf

---
+++ HW5 - Code/PCA_ass05_part2.m
@@ -0,0 +1,8 @@
+clear all
+close all
+
+%% Computation of Tau_{k}^{coag}
+
+%% Computation of Tao_{k,k+1}^{cond}
+
+%% Computation of Pr_{k->k+1}
--- HW5 - Code/coag_loss_coef.m
+++ HW5 - Code/coag_loss_coef.m
@@ -6,12 +6,10 @@
  kappa = zeros(size(Dp));
 
  for k = 1:kmax
-
      j = (k+1):kmax;
      for i = 1:j
         kappa(k) = kappa(k) + n(k)*coag_kernel(Dp(k),Dp(i))*n(i);
      end
-
  end
 
  %% -----------------------------------------------------------------------------
