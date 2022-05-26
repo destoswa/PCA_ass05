81623fcec38390c3ca75804b6976a2bc6e8145dc
destoswa <84578314+destoswa@users.noreply.github.com>
2022-05-27 00:17:45 +0200

    avancées sur 1.2

--- HW5 - Code/PCA_ass05_part2.m
+++ HW5 - Code/PCA_ass05_part2.m
@@ -7,9 +7,14 @@ N0 = 200; %[cm^-3]
 N = [50,200,500]; %[cm^-3]
 gradp0 = 1e-9; %[atm]
 %% Computation of Tau_{k}^{coag}
-tao_coag = ndist./coag_loss_coef(ndist,Dp);
+tao_coag = 1./coag_loss_coef(ndist,Dp);
 figure(1)
 semilogy(Dp,tao_coag,'.')
 %% Computation of Tao_{k,k+1}^{cond}
-
+tao_cond = zeros(length(Dp)-1,1);
+for i=1:length(tao_cond)
+    tao_cond(i) = Dp(i+1)-Dp(i)/(growth_rate(Dp(i+1),dDp(i+1))-growth_rate(Dp(i),dDp(i)));
+end
+figure(2)
+semilogy(Dp(1:end-1),tao_cond,'.')
 %% Computation of Pr_{k->k+1}
--- HW5 - Code/coag_loss_coef.m
+++ HW5 - Code/coag_loss_coef.m
@@ -7,9 +7,9 @@
 
  for k = 1:kmax
      j = (k+1):kmax;
-     kappa(k) = 0.5*coag_kernel(Dp(k),Dp(k))*n(k)^2;
+     kappa(k) = 0.5*coag_kernel(Dp(k),Dp(k))*n(k);
      for i = 1:j
-        kappa(k) = kappa(k) + n(k)*coag_kernel(Dp(k),Dp(i))*n(i);
+        kappa(k) = kappa(k) + coag_kernel(Dp(k),Dp(i))*n(i);
      end
  end
 
